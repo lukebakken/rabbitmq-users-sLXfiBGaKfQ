0f6639cb4ac38d1387ef00360589ee237f3f8e91
Luke Bakken <luke@bakken.io>
2022-06-23 12:07:11 -0700

    SSL connection works. No trust store in use just yet

--- client.py
+++ client.py
@@ -1,56 +1,48 @@
-# -*- coding: utf-8 -*-
-# pylint: disable=C0111,C0103,R0205
-
 import logging
-import threading
 import pika
-import random
+import signal
 import ssl
+import sys
 import time
+import traceback
 
-LOG_FORMAT = (
-    "%(levelname) -10s %(asctime)s %(name) -30s %(funcName) "
-    "-35s %(lineno) -5d: %(message)s"
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+logger.info("ssl.HAS_SNI: %s", ssl.HAS_SNI)
+context = ssl.create_default_context(cafile="./certs/ca_certs.pem")
+context.load_cert_chain("./certs/rmq-1/client_rmq-1_certificate.pem", "./certs/rmq-1/client_rmq-1_key.pem")
+ssl_options = pika.SSLOptions(context, server_hostname="rmq-0")
+conn_params = pika.ConnectionParameters(
+    host="rmq-0", port=5671, ssl_options=ssl_options
 )
-LOGGER = logging.getLogger(__name__)
 
-logging.basicConfig(level=logging.ERROR, format=LOG_FORMAT)
-
-stopping = False
-
-threads = []
+sigterm_caught = False
 
 
-def do_work(i):
-    while True and not stopping:
-        context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-        context.verify_mode = ssl.CERT_REQUIRED
-        context.load_verify_locations('./cluster/certs/ca_certificate.pem')
-
-        thread_id = threading.get_ident()
-        LOGGER.info("i: %s thread id: %s", i, thread_id)
-        credentials = pika.PlainCredentials("bakkenl", "test1234")
-        p = 5682 + (i % 3)
-        parameters = pika.ConnectionParameters(
-            host="localhost", port=p,
-            credentials=credentials, heartbeat=5,
-            ssl_options=pika.SSLOptions(context)
-        )
-        connection = pika.BlockingConnection(parameters)
-        for i in range(1, random.randrange(5, 10)):
-            connection.channel()
-        connection.sleep(random.randrange(5, 10))
-        connection.close()
+def sigterm_handler(_signo, _stack_frame):
+    logger.info("Saw SIGTERM, exiting...")
+    sigterm_caught = True
 
 
-for i in range(0, 63):
-    t = threading.Thread(target=do_work, args=(i,))
-    t.start()
-    threads.append(t)
+signal.signal(signal.SIGTERM, sigterm_handler)
 
-input("ANY KEY TO STOP")
-print()
-stopping = True
-# Wait for all to complete
-for thread in threads:
-    thread.join()
+while not sigterm_caught:
+    try:
+        with pika.BlockingConnection(conn_params) as conn:
+            ch = conn.channel()
+            ch.queue_declare("foobar")
+            ch.basic_publish("", "foobar", "Hello, world!")
+            logger.info(ch.basic_get("foobar"))
+            logger.info("sleeping 10 seconds...")
+            for _x in range(10):
+                if sigterm_caught:
+                    break
+                conn.sleep(1)
+    except KeyboardInterrupt:
+        logger.error("CAUGHT CTRL-C, exiting!")
+        sys.exit(0)
+    except:
+        logger.error("CAUGHT EXCEPTION:")
+        logger.error(traceback.format_exc())
+        logger.info("sleeping 5 seconds...")
+        time.sleep(5)
--- init.sh
+++ init.sh
@@ -8,6 +8,9 @@ readonly dir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
 git submodule update --init
 
+readonly all_ca_certs_file="$dir/certs/ca_certs.pem"
+rm -vf "$all_ca_certs_file"
+
 for ((i = 0; i < 2; i++))
 do
     ca_certfile="$dir/certs/rmq-$i/ca_certificate.pem"
@@ -18,29 +21,18 @@ do
         make -C "$dir/tls-gen/basic" "CN=rmq-$i"
         cp -v "$dir/tls-gen/basic/result/"*.pem "$dir/certs/rmq-$i"
     fi
+
+    if [[ -s $ca_certfile ]]
+    then
+        cat "$ca_certfile" >> "$all_ca_certs_file"
+    else
+        echo "[ERROR] expected file '$ca_certfile' to exist!" 2>&1
+        exit 1
+    fi
 done
 
-# docker build --tag rabbitmq-users-slxfibgakfq:latest --file "$dir/docker/rmq/Dockerfile" .
+export DOCKER_BUILDKIT=1
 
-docker build --tag rabbitmq-users-slxfibgakfq-pika:latest --file "$dir/docker/pika/Dockerfile" .
+docker build --pull --tag rabbitmq-users-slxfibgakfq:latest --file "$dir/docker/rmq/Dockerfile" .
 
-## {
-##     cd "$dir/rabbitmq-server" && asdf local erlang 24.3.4 && asdf local elixir 1.12.3-otp-24
-##     make FULL=1
-## } &
-## 
-## make -C "$dir/rabbitmq-perf-test" compile &
-## 
-## wait
-## 
-## make -C "$dir/rabbitmq-server" RABBITMQ_CONFIG_FILE="$dir/rabbitmq.conf" PLUGINS='rabbitmq_management rabbitmq_top' NODES=3 start-cluster
-## 
-## (cd "$dir/rabbitmq-server" && ./sbin/rabbitmqctl --node rabbit-1 set_policy --apply-to queues \
-##     --priority 0 policy-0 ".*" '{"ha-mode":"all", "ha-sync-mode": "automatic", "queue-mode": "lazy"}')
-## 
-## (cd "$dir/rabbitmq-server" && ./sbin/rabbitmqctl --node rabbit-1 set_policy --apply-to queues \
-##     --priority 1 policy-1 ".*" '{"ha-mode":"all", "ha-sync-mode": "automatic"}')
-## 
-## make -C "$dir/rabbitmq-perf-test" ARGS='--consumers 0 --producers 1 --predeclared --queue gh-5086 --pmessages 1500000 --size 1024' run
-## 
-## (cd "$dir/rabbitmq-server" && ./sbin/rabbitmqctl --node rabbit-1 clear_policy policy-1)
+docker build --pull --tag rabbitmq-users-slxfibgakfq-pika:latest --file "$dir/docker/pika/Dockerfile" .
